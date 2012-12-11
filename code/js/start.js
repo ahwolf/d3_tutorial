701284b786b5fe0820c9dbc27ad72c9cfb2f6cac
Aaron Wolf <aaron.wolf@datascopeanalytics.com>
2012-12-10 20:13:12 -0600

    added css file for xcharts

--- code/js/start.js
+++ code/js/start.js
@@ -1,103 +1,61 @@
-var height = 500;
-var width = 500;
-var padding = 60;
+var tt = document.createElement('div'),
+  leftOffset = -(~~$('html').css('padding-left').replace('px', '') + ~~$('body').css('margin-left').replace('px', '')),
+  topOffset = -32;
+tt.className = 'ex-tooltip';
+document.body.appendChild(tt);
 
-var svg = d3.select("#main_container")
-    .append("svg")
-    .attr("width",width)
-    .attr("height",height);
+var data = {
+  "xScale": "time",
+  "yScale": "linear",
+  "main": [
+    {
+      "className": ".pizza",
+      "data": [
+        {
+          "x": "2012-11-05",
+          "y": 6
+        },
+        {
+          "x": "2012-11-06",
+          "y": 6
+        },
+        {
+          "x": "2012-11-07",
+          "y": 8
+        },
+        {
+          "x": "2012-11-08",
+          "y": 3
+        },
+        {
+          "x": "2012-11-09",
+          "y": 4
+        },
+        {
+          "x": "2012-11-10",
+          "y": 9
+        },
+        {
+          "x": "2012-11-11",
+          "y": 6
+        }
+      ]
+    }
+  ]
+};
+var opts = {
+  "dataFormatX": function (x) { return d3.time.format('%Y-%m-%d').parse(x); },
+  "tickFormatX": function (x) { return d3.time.format('%A')(x); },
+  "mouseover": function (d, i) {
+    var pos = $(this).offset();
+    $(tt).text(d3.time.format('%A')(d.x) + ': ' + d.y)
+      .css({top: topOffset + pos.top, left: pos.left + leftOffset})
+      .show();
+  },
+  "mouseout": function (x) {
+    $(tt).hide();
+  }
+};
+var myChart = new xChart('line-dotted', data, '#myChart', opts);
 
-var data = [1,2,3,4,5,6,7];
-var color = ["black","red","blue","yellow",
-	     "green","grey","purple"];
 
-var x_scale = d3.scale.linear()
-    .domain(d3.extent(data, function(d)
-		      {return d}))
-    .range([padding,width - padding]);
-
-var y_scale = d3.scale.linear()
-    .domain(d3.extent(data, function(d){
-	return d}))
-    .range([height-padding, padding]);
-
-var x_axis = d3.svg.axis()
-    .scale(x_scale)
-    .orient("bottom")
-    .ticks(8);
-
-var y_axis = d3.svg.axis()
-    .scale(y_scale)
-    .orient("left")
-    .ticks(8);
-
-// Added the y-axis to the dom
-svg.append("g")
-    .attr("transform","translate(" 
-	  + padding + ",0)")
-    .attr("class","y_axis")
-    .call(y_axis);
-
-// Added the x-axis to the dom
-svg.append("g")
-    .attr("transform","translate(0," 
-	  + (height - padding) + ")")
-    .attr("class","x_axis")
-    .call(x_axis);
-
-var circles = svg.selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-    .attr("cx",function(d,i){
-	return x_scale(d)})
-    .attr("cy",function(d){
-	return y_scale(d)})
-    .attr("fill",function (d,i){
-	console.log(d,i);
-	return color[i];})
-    .attr("r", function(d) {
-	return x_scale(d)/10});
-
-var data_2 = [3, 5, 7, 1, 3]
-
-var new_circles = svg.selectAll("circle")
-    .data(data_2);
-
-var max = d3.max(data, function(d){return d});
-console.log(max);
-new_circles.transition()
-    .delay(1000)
-    .duration(2500)
-    // .ease("linear")
-    .attr("cx", function(d,i){
-	if (d === max){
-	    return x_scale(d)
-	}
-	else{
-	    return x_scale(d)
-	}
-    })
-    .attr("cy", function(d,i){
-	if (d === max){
-	    return x_scale(d)
-	}
-	else{
-	    return y_scale(d)
-	}
-    })
-    .attr("fill", function (d,i){
-	if (d === max){
-	    return color[i];
-	}
-	else{
-	    // var index = 6 - i;
-	    return color[6 - i];
-	}
-    });
-
-new_circles.exit()
-    .transition()
-    .delay(1000)
-    .duration(2500)
-    .attr("r",0)
-    .remove();
