a3bd26fc4c35bb7cd2d12c4898ce14acf8753e14
Aaron Wolf <aaron.wolf@datascopeanalytics.com>
2013-01-06 22:00:45 -0600

    messing with projections:

--- code/js/lib/util-geo.js
+++ code/js/lib/util-geo.js
@@ -8,19 +8,23 @@ geons.geoConfig = function() {
     this.origin = appConstants.origin;
 
     this.mercator = d3.geo.mercator();
-    this.albers = d3.geo.albers();
-
+    var wtf = this
+    this.albers = d3.geo.albers()
+	.scale(wtf.SCALE)
+	.origin(wtf.origin)
+	.translate([wtf.TRANSLATE_0,wtf.TRANSLATE_1]);
+    
     this.path = d3.geo.path().projection(this.albers);
 
-    this.setupGeo = function() {
-        var translate = this.albers.translate();
-        translate[0] = this.TRANSLATE_0;
-        translate[1] = this.TRANSLATE_1;
+    // this.setupGeo = function() {
+    //     var translate = this.albers.translate();
+    //     translate[0] = this.TRANSLATE_0;
+    //     translate[1] = this.TRANSLATE_1;
 
-        this.albers.translate(translate);
-        this.albers.scale(this.SCALE);
-	this.albers.origin(this.origin);
-    }
+    //     this.albers.translate(translate);
+    //     this.albers.scale(this.SCALE);
+    // 	this.albers.origin(this.origin);
+    // }
 }
 
 // geoConfig contains the configuration for the geo functions
