490d9f39881776ac3b08e3d6774a464da75edf31
Aaron Wolf <aaron.wolf@datascopeanalytics.com>
2013-01-06 21:31:06 -0600

    added libraries :

--- code/js/snowflake.js
+++ code/js/snowflake.js
@@ -50,6 +50,84 @@ var y_scale = d3.scale.linear()
     .domain([0,height])
     .range([height, 0]);
 
+var gradient = svg.append("svg:defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient")
+    .attr("x1", "0%")
+    .attr("y1", "0%")
+    .attr("x2", "50%")
+    .attr("y2", "50%")
+    .attr("spreadMethod", "pad");
+
+gradient.append("svg:stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#42cae9")
+    .attr("stop-opacity", 1);
+
+gradient.append("svg:stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#9ee4f3")
+    .attr("stop-opacity", 1);
+
+var gradient_2 = svg.append("svg:defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient_2")
+    .attr("x1", "50%")
+    .attr("y1", "50%")
+    .attr("x2", "100%")
+    .attr("y2", "100%")
+    .attr("spreadMethod", "pad");
+
+gradient_2.append("svg:stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#9ee4f3")
+    .attr("stop-opacity", 1);
+
+gradient_2.append("svg:stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#42cae9")
+    .attr("stop-opacity", 1);
+
+var gradient_3 = svg.append("svg:defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient_3")
+    .attr("x1", "100%")
+    .attr("y1", "0%")
+    .attr("x2", "50%")
+    .attr("y2", "50%")
+    .attr("spreadMethod", "pad");
+
+gradient_3.append("svg:stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#42cae9")
+    .attr("stop-opacity", 1);
+
+gradient_3.append("svg:stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#9ee4f3")
+    .attr("stop-opacity", 1);
+
+var gradient_4 = svg.append("svg:defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient_4")
+    .attr("x1", "0%")
+    .attr("y1", "100%")
+    .attr("x2", "100%")
+    .attr("y2", "0%")
+    .attr("spreadMethod", "pad");
+
+gradient_4.append("svg:stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#42cae9")
+    .attr("stop-opacity", 1);
+
+gradient_4.append("svg:stop")
+    .attr("offset", "50%")
+    .attr("stop-color", "#9ee4f3")
+    .attr("stop-opacity", 1);
+
+
+
 initial_draw();
 
 function initial_draw(){    
@@ -60,7 +138,8 @@ function initial_draw(){
 	.attr("class","path_1")
 	.attr("class","snowflake")
 	.attr("d",line);
-    
+
+    svg.selectAll("circle").remove();
     var circles = svg.selectAll("circle")
 	.data(data_1)
 	.enter().append("circle")
@@ -119,15 +198,14 @@ function reset_snowflake(){
 }
 
 function animate_snowflake(){
-    
     var circles = svg.selectAll("circle")
 	.remove();
     d3.select(".snowflake").transition().duration(0)
-	.attr("fill","green");
+	.attr("fill","url(#gradient)");
     var path_2 = svg.selectAll(".path_2")
 	.data([data_1])
     path_2.enter().append("path")
-	.attr("fill","green")
+	.attr("fill","none")
 	.attr("class","path_2")
 	.attr("class","snowflake")
 	.attr("d",function (d){
@@ -135,39 +213,34 @@ function animate_snowflake(){
 	    return line(d);})
 	.transition().duration(duration)
 	.attr("d",first_flip)
-	.attr("fill","red");
-    
-	
-    // console.log(path_1);
+	.attr("fill","url(#gradient_2)");
+    	
     setTimeout(function(){
 	var data_2 = create_data(path_2);
 	var path_3 = svg.selectAll(".path_3")
 	    .data([data_1]);
 	
 	path_3.enter().append("path")
-	    .attr("fill","green")//"rgba(120,120,120,1)")
 	    .attr("class","path_3")
 	    .attr("class","snowflake")
 	    .attr("d",line);
+
 	
 	path_3.transition().duration(duration)
     	    .attr("d", second_flip)
-	
-	
-	
+	    .attr("fill","url(#gradient_3)");//"rgba(120,120,120,1)")
+		
 	var path_4 = svg.selectAll(".path_4")
 	    .data([data_2]);
 	
 	path_4.enter().append("path")
-	.attr("fill","red")//"rgba(120,120,120,1)")
 	    .attr("class","path_4")
 	    .attr("class","snowflake")
 	    .attr("d",line);
 	
 	path_4.transition().duration(duration)
     	    .attr("d", second_flip)
-	
-	
+	    .attr("fill","url(#gradient_4)");//"rgba(120,120,120,1)")
 	setTimeout(function(){
 	    var data_3 = create_data(path_3);
 	    var data_4 = create_data(path_4);
@@ -176,50 +249,81 @@ function animate_snowflake(){
 		.data([data_1]);
 	    
 	    path_5.enter().append("path")
-		.attr("fill","green")//"rgba(120,120,120,1)")
+
 		.attr("class","path_5")
 		.attr("class","snowflake")
 		.attr("d",line);
 	    
 	    path_5.transition().duration(duration)
-    		.attr("d", third_flip);
-	    
+    		.attr("d", third_flip)
+		.attr("fill","url(#gradient_4)");//"rgba(120,120,120,1)")	    
 	    var path_8 = svg.selectAll(".path_8")
 		.data([data_4]);
 	    
 	    path_8.enter().append("path")
-		.attr("fill","red")//"rgba(120,120,120,1)")
+
 		.attr("class","path_8")
 		.attr("class","snowflake")
 		.attr("d",line);
 	    
 	    path_8.transition().duration(duration)
     		.attr("d", third_flip)
-	    
+		.attr("fill","url(#gradient)");//"rgba(120,120,120,1)")	    
 	    var path_7 = svg.selectAll(".path_7")
 		.data([data_3]);
 	    
 	    path_7.enter().append("path")
-		.attr("fill","green")//"rgba(120,120,120,1)")
+
 		.attr("class","path_7")
 		.attr("class","snowflake")
 		.attr("d",line);
 	    
 	    path_7.transition().duration(duration)
-    		.attr("d", third_flip);
-	    
+    		.attr("d", third_flip)
+		.attr("fill","url(#gradient_2)");
 	    var path_6 = svg.selectAll(".path_6")
 		.data([data_2]);
 	    
 	    path_6.enter().append("path")
-		.attr("fill","red")//"rgba(120,120,120,1)")
+
 		.attr("class","path_6")
 		.attr("class","snowflake")
 		.attr("d",line);
 	    
 	    path_6.transition().duration(duration)
     		.attr("d", third_flip)
-	    
+		.attr("fill","url(#gradient_3)");
+	    console.log(data_1,data_2,data_3,data_4);
+	    var circle_data = data_1.concat(data_2,data_3,data_4);
+	    var new_circle = [];
+	    _.each(circle_data,function(d){
+		new_circle.push([d[0],height-d[1]]);
+	    });
+	    circle_data = circle_data.concat(new_circle);
+	    console.log(circle_data);
+	    svg.selectAll("circle")
+		.data(circle_data)
+		.enter().append("circle")
+		.attr("cx",function(d){console.log(d);
+				       return d[0]})
+		.attr("cy",function(d){return d[1]})
+		.attr("r",2)
+		.attr("fill","none");
+	    d3.timer(function(time){
+		var opacity_scale = [0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
+		var circle = svg.selectAll("circle")
+		    .data(circle_data);
+		circle.transition().duration(5)
+		    .attr("fill",function(d,i){
+			var random_metric = Math.random();
+			return "rgba(255,255,255,"+random_metric+")";
+		    });	
+		console.log(time);
+		if ($("#animation_button").html() == "Create Snowflake"){
+		    svg.selectAll("circle").remove();
+		    return true;
+		}
+	    });
 	}, duration);
 	
     }, duration);
---
+++ code/js/ward_drawing.js
@@ -0,0 +1,161 @@
+var w = 960;
+var h = 800;
+var padding = 40;
+
+var albers = d3.geo.albers()
+    .scale(80000)
+    .origin([-87.63073,41.836084])
+    .translate([400,400]);
+
+var azimuthal = d3.geo.azimuthal()
+    .scale(1)
+    .origin([-87.63073,41.836084]);
+
+var path = d3.geo.path().projection(albers);
+
+// Old rendering, no used for three.js
+var vis = d3.select("#main_container")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+var data = ward;
+
+// calculate the max and min of all the property values
+var gas_min_max = d3.extent(data.features, function(feature){
+    return feature.properties.gas
+});
+
+var elec_min_max = d3.extent(data.features, function(feature){
+    return feature.properties.elect
+});
+
+var gas_eff_min_max = d3.extent(data.features, function(feature){
+
+    return feature.properties.gas_efficiency
+});
+
+var elec_eff_min_max = d3.extent(data.features, function(feature){
+    return feature.properties.elect_efficiency
+});
+
+var color_scale = d3.scale.linear()
+    .domain(elec_eff_min_max)
+    .range(['red','blue']);
+
+vis.selectAll("path")
+    .data(data.features)
+    .enter().append("path")
+    .attr('fill', function(d){return color_scale(d.properties.elect_efficiency);})
+    .attr('stroke',"black")
+    .attr("d", path);
+    // .on("mouseover", function(d) {
+    // 	console.log(d);
+
+
+// The following code was copied from
+// http://www.smartjava.org/content/render-geographic-information-3d-threejs-and-d3js
+// and updated to take advantage of built in d3 tools
+
+var scene;
+var renderer;
+var camera;
+
+initScene();
+addGeoObject();
+renderer.render( scene, camera );
+
+// Set up the three.js scene. This is the most basic setup without
+// any special stuff
+function initScene() {
+    // set the scene size
+    var WIDTH = 960, HEIGHT = 800;
+
+    // set some camera attributes
+    var VIEW_ANGLE = 45, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 10000;
+
+    // create a WebGL renderer, camera, and a scene
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT,
+                                         NEAR, FAR);
+    scene = new THREE.Scene();
+    
+    // add and position the camera at a fixed position
+    scene.add(camera);
+    camera.position.z = 550;
+    camera.position.x = 0;
+    camera.position.y = 550;
+    camera.lookAt( scene.position );
+    
+    // start the renderer, and black background
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.setClearColor(0x000);
+    
+    // add the render target to the page
+    $("#main_container").append(renderer.domElement);
+
+    // add a light at a specific position
+    var pointLight = new THREE.PointLight(0xFFFFFF);
+    scene.add(pointLight);
+    pointLight.position.x = 800;
+    pointLight.position.y = 800;
+    pointLight.position.z = 800;
+    
+    // add a base plane on which we'll render our map
+    var planeGeo = new THREE.PlaneGeometry(10000, 10000, 10, 10);
+    var planeMat = new THREE.MeshLambertMaterial({color: 0x666699});
+    var plane = new THREE.Mesh(planeGeo, planeMat);
+
+    // rotate it to correct position
+    plane.rotation.x = -Math.PI/2;
+    scene.add(plane);
+}
+
+
+// add the loaded gis object (in geojson format) to the map
+function addGeoObject() {
+    // keep track of rendered objects
+    var meshes = [];
+    
+    // convert to mesh and calculate values
+    _.each(data.features, function (geoFeature){	
+        var feature = geo.path(geoFeature);
+        // we only need to convert it to a three.js path
+        var mesh = transformSVGPathExposed(feature);
+        // add to array
+        meshes.push(mesh);
+	
+	var color_scale = d3.scale.linear()
+	    .domain(gas_eff_min_max)
+	    .range(['red','blue']);
+	
+	var extrude_scale = d3.scale.linear()
+	    .domain(elec_eff_min_max)
+	    .range([0,100]);
+
+        // create material color based on gas efficiency
+        var mathColor = color_scale(geoFeature.properties.gas_efficiency);
+        var material = new THREE.MeshLambertMaterial({
+            color: mathColor
+        });
+	
+        // create extrude based on electricity efficiency
+        var extrude = extrude_scale(geoFeature.properties.elec_efficiency);
+        var shape3d = meshes[i].extrude({amount: Math.round(extrude), 
+					 bevelEnabled: false
+					});
+	
+        // create a mesh based on material and extruded shape
+        var toAdd = new THREE.Mesh(shape3d, material);
+	
+        // rotate and position the elements nicely in the center
+        toAdd.rotation.x = Math.PI/2;
+        toAdd.translateX(-490);
+        toAdd.translateZ(50);
+        toAdd.translateY(extrude/2);
+	
+        // add to scene
+        scene.add(toAdd);
+    });
+}
+
